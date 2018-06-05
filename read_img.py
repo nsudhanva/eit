f1a671ed8d0c51d802b684ba3ca6282a6e138b02
Sudhanva <nsudhanva@gmail.com>
2018-06-05 18:13:17 +0530

    Random Image collapse - rebuild

--- read_img.py
+++ read_img.py
@@ -1,21 +1,25 @@
 import numpy as np
-import pandas as pd
+import scipy.misc
 import matplotlib.pyplot as plt
+from IPython import get_ipython
+get_ipython().run_line_magic('matplotlib', 'qt5')
+
+matrix1 = np.random.uniform(low=0, high=1, size=100)
+matrix2 = np.random.uniform(low=0, high=1, size=100)
+
+matrix = np.array([matrix1, matrix2])
 
 img = plt.imread('eitcrop.png')
-shape = img.shape[0]
-numbers = list(range(1, shape))
-plt.axis('off')
-plt.imshow(img)
-
 collapsed = img.mean(axis=2)
-x = list(collapsed[0])
-y = list(collapsed[1])
-plt.hist2d(x, y)
-plt.colorbar()
-plt.show()
 
-print(collapsed.shape)
+x = collapsed.ravel()
+y = img.ravel()
+
+
+#plt.hist(x, edgecolor='black', bins = list(np.arange(0.0, 1.0, 0.05)))
+#plt.savefig('histogram.jpg')
+#plt.show()
+
 plt.set_cmap('gray')
 plt.imshow(collapsed, cmap='gray')
 plt.axis('off')
---
+++ two_d.py
@@ -0,0 +1,6 @@
+import numpy as np 
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([7, 8, 2, 4, 1])
+
+z = np.array([x, y])
+print(z)
