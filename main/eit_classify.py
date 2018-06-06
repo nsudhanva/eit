e4d91db7230f2227a5832224b2d9af036d0cf217
Sudhanva <nsudhanva@gmail.com>
2018-06-06 13:52:06 +0530

    Classify dictr

--- main/eit_classify.py
+++ main/eit_classify.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.misc
 import matplotlib.pyplot as plt
 import os
+from collections import OrderedDict
 from IPython import get_ipython
 get_ipython().run_line_magic('matplotlib', 'qt5')
 
@@ -10,11 +11,29 @@ PARENT_DIR = os.path.abspath(os.path.join(ROOT_DIR, os.pardir))
 
 img = plt.imread(PARENT_DIR + '\\assets\\eitcrop.png')
 img_two_d = img.mean(axis=2)
+img_one_d = img_two_d.ravel()
 
-clrs = ('brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'gray')
-cp = plt.contourf(img_two_d, colors=clrs)
-plt.colorbar(cp)
-plt.title('Filled Contours Plot')
-plt.xlabel('Pixels(x)')
-plt.ylabel('Pixels(y)')
+colors = ['brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'gray']
+classify_dict = {}
+low = 0.0
+high = 1.1
+skip = 0.1
+
+intensity = np.arange(low, high, skip)
+color_length = high / len(colors)
+color_length_array = np.full((1, len(intensity) - 1), round(color_length, 2))
+color_length_array = np.insert(color_length_array, 0, 0)
+intensity_range = np.cumsum(color_length_array)
+
+for index, color in enumerate(colors):
+    # print(intensity_range[index], intensity_range[index + 1])
+    classify_dict[color] = np.where(np.logical_and(img_one_d >= intensity_range[index], img_one_d < intensity_range[index + 1]))[0]
+
+classify_dict_count = {}
+
+for key, value in classify_dict.items():
+    classify_dict_count[key] = len(value)
+
+plt.bar()
+plt.xticks()
 plt.show()
